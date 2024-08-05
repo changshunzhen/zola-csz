b371fe1e3faffeca35e6014a7215c3ec6e5a1e4e
zhangyuan <zhangyuan@users.noreply.github.com>
2024-08-05 21:42:52 +0800

    add sidebar style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
+  safelist: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
